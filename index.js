b0ad330add34dedbe0e9442a01b7beef4d002a1e
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-01-08 18:27:31 +0600

    update 2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 // middleware
@@ -9,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://taskMaster:fa0rPNlqX8UEoEv6@taskmaster.xualaxs.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
